532042cebbbbda311f7358e8be21ec8048b85fd2
vinhpooh <pham.hoang.vinh@gmail.com>
2018-04-24 15:32:27 +0200

    suppression import inutile

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/components/home/home.component';
-import { PageNotFoundComponent } from './page-not-found/components/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '',
