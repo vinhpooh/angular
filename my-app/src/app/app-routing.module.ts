da0f4b034fc1f977639a0a8c2c61f1cf4ec53301
vinhpooh <pham.hoang.vinh@gmail.com>
2018-04-25 14:23:18 +0200

    preload

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   {
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       {
+        preloadingStrategy: PreloadAllModules
         // enableTracing: true // <-- debugging purposes only
       }
     )
